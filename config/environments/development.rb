f2699539b8c583cce45941c3ba06448370697ffb
bricejlin <bricejlin@gmail.com>
2013-10-10 01:48:31 -0400

    font-face path change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,6 @@ Pipetrack::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  config.assets.precompile += %w(.svg .eot .woff .ttf)
+  # config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  # config.assets.precompile += %w(.svg .eot .woff .ttf)
 end
