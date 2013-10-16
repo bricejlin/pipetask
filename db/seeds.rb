0c2b1644010d3317cd1ace079ce7b89099d96062
bricejlin <bricejlin@gmail.com>
2013-10-16 17:37:06 -0400

    added notice on index

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,61 +11,61 @@ Pipe.create!([
 		name: 'Example Pipe Uno',
 		sides_shot: 3,
 		images_attributes: [
-			{ url: 'chrispipes-1.jpg' },
-			{ url: 'chrispipes-2.jpg' },
-			{ url: 'chrispipes-3.jpg' }
+			{ url: 'pipe-1.jpg' },
+			{ url: 'pipe-2.jpg' },
+			{ url: 'pipe-3.jpg' }
 		]
 	},
 	{
 		name: 'Example Pipe Dos',
 		sides_shot: 3,
 		images_attributes: [
-			{ url: 'chrispipes-4.jpg' },
-			{ url: 'chrispipes-5.jpg' },
-			{ url: 'chrispipes-6.jpg' }	
+			{ url: 'pipe-4.jpg' },
+			{ url: 'pipe-5.jpg' },
+			{ url: 'pipe-6.jpg' }	
 		]
 	},
 	{
 		name: 'Example Pipe Tres',
 		sides_shot: 3,
 		images_attributes: [
-			{ url: 'chrispipes-7.jpg' },
-			{ url: 'chrispipes-8.jpg' },
-			{ url: 'chrispipes-9.jpg' }	
+			{ url: 'pipe-7.jpg' },
+			{ url: 'pipe-8.jpg' },
+			{ url: 'pipe-9.jpg' }	
 		]	
 	},
 	{
 		name: 'Example Pipe Cuatro',
 		sides_shot: 3,
 		images_attributes: [
-			{ url: 'chrispipes-10.jpg' },
-			{ url: 'chrispipes-11.jpg' }
+			{ url: 'pipe-10.jpg' },
+			{ url: 'pipe-11.jpg' }
 		]		
 	},
 	{
 		name: 'Example Pipe Cinco',
 		sides_shot: 9,
 		images_attributes: [
-			{ url: 'tyn-1.jpg' },
-			{ url: 'tyn-2.jpg' },
-			{ url: 'tyn-3.jpg' },
-			{ url: 'tyn-4.jpg' },
-			{ url: 'tyn-5.jpg' },
-			{ url: 'tyn-6.jpg' },
-			{ url: 'tyn-7.jpg' }
+			{ url: 'pipe-1.jpg' },
+			{ url: 'pipe-2.jpg' },
+			{ url: 'pipe-3.jpg' },
+			{ url: 'pipe-4.jpg' },
+			{ url: 'pipe-5.jpg' },
+			{ url: 'pipe-6.jpg' },
+			{ url: 'pipe-7.jpg' }
 		]	
 	},
 	{
 		name: 'Example Pipe Seis',
 		sides_shot: 3,
 		images_attributes: [
-			{ url: 'tynn-01.jpg' },
-			{ url: 'tynn-02.jpg' },
-			{ url: 'tynn-03.jpg' },
-			{ url: 'tynn-04.jpg' },
-			{ url: 'tynn-05.jpg' },
-			{ url: 'tynn-06.jpg' },
-			{ url: 'tynn-07.jpg' }
+			{ url: 'pipe-11.jpg' },
+			{ url: 'pipe-12.jpg' },
+			{ url: 'pipe-13.jpg' },
+			{ url: 'pipe-14.jpg' },
+			{ url: 'pipe-15.jpg' },
+			{ url: 'pipe-16.jpg' },
+			{ url: 'pipe-17.jpg' }
 		]	
 	}
 ])
