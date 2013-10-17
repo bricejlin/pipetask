3684cf4705b23b02c59d5a900d30df98ab6b5e09
bricejlin <bricejlin@gmail.com>
2013-10-16 20:38:38 -0400

    add images

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,5 +1,19 @@
 module AdminHelper
-	def pipes_with_sides(sides)
-		@pipes.where(sides_shot: sides).count
+	def receivable(sides)
+		if sides == 3
+			number_to_currency(3 * sides_pipe_count(sides, 'approved'))
+		elsif sides = 9
+			number_to_currency(4 * sides_pipe_count(sides, 'approved'))
+		end
+	end
+
+	def sides_pipe_count(sides, status='all')
+		if status == 'approved'
+			@pipes.where(sides_shot: sides, approved: true).count
+		elsif status == 'unapproved'
+			@pipes.where(sides_shot: sides, approved: false).count
+		else
+			@pipes.where(sides_shot: sides).count
+		end
 	end
 end
