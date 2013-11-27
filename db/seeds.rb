9157153bded3806bf35f3e9929806e27a0d6b93a
bricejlin <bricejlin@gmail.com>
2013-11-26 21:16:02 -0500

    changed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,70 +11,7 @@ User.create!([
 		last_name: 'Lin',
 		email: 'bricejlin@gmail.com',
 		password: 'foo',
-		password_confirmation: 'foo'
-	}
-])
-
-Pipe.create!([
-	{
-		name: 'Example Pipe Uno',
-		sides_shot: 3,
-		images_attributes: [
-			{ url: 'pipe-46.jpg' },
-			{ url: 'pipe-47.jpg' },
-			{ url: 'pipe-48.jpg' }
-		]
-	},
-	{
-		name: 'Example Pipe Dos',
-		sides_shot: 3,
-		images_attributes: [
-			{ url: 'pipe-49.jpg' },
-			{ url: 'pipe-50.jpg' },
-			{ url: 'pipe-51.jpg' }	
-		]
-	},
-	{
-		name: 'Example Pipe Tres',
-		sides_shot: 3,
-		images_attributes: [
-			{ url: 'pipe-83.jpg' },
-			{ url: 'pipe-84.jpg' },
-			{ url: 'pipe-85.jpg' }	
-		]	
-	},
-	{
-		name: 'Example Pipe Cuatro',
-		sides_shot: 3,
-		images_attributes: [
-			{ url: 'pipe-90.jpg' },
-			{ url: 'pipe-91.jpg' }
-		]		
-	},
-	{
-		name: 'Example Pipe Cinco',
-		sides_shot: 9,
-		images_attributes: [
-			{ url: 'a_tynnubill_1-01.jpg' },
-			{ url: 'a_tynnubill_1-02.jpg' },
-			{ url: 'a_tynnubill_1-03.jpg' },
-			{ url: 'a_tynnubill_1-04.jpg' },
-			{ url: 'a_tynnubill_1-05.jpg' },
-			{ url: 'a_tynnubill_1-06.jpg' },
-			{ url: 'a_tynnubill_1-07.jpg' }
-		]	
-	},
-	{
-		name: 'Example Pipe Seis',
-		sides_shot: 3,
-		images_attributes: [
-			{ url: 'pipe-91.jpg' },
-			{ url: 'pipe-92.jpg' },
-			{ url: 'pipe-93.jpg' },
-			{ url: 'pipe-94.jpg' },
-			{ url: 'pipe-95.jpg' },
-			{ url: 'pipe-96.jpg' },
-			{ url: 'pipe-97.jpg' }
-		]	
+		password_confirmation: 'foo',
+		admin: true
 	}
 ])
